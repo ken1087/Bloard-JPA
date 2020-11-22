1742a89f64b704141e9b3a8e966fb24492206799
in kang <ken940816@gmail.com>
2020-11-22 13:07:37 +0900

    Create Model

--- src/main/java/com/blog/model/Board.java
+++ src/main/java/com/blog/model/Board.java
@@ -1,11 +1,21 @@
 package com.blog.model;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.Data;
 
 @Entity
 @Data
 public class Board {
-
+	private int id;
+	private String title;
+	private String content;
+	@CreationTimestamp
+	private LocalDate createDate;
+	@CreationTimestamp
+	private LocalDate updateDate;
 }
